1b5267d757a3aed1e97c3f186563856561f353f1
Vysakh Premkumar <84713473+tellmeY18@users.noreply.github.com>
2025-01-09 22:57:41 +0530

    minified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,55 +1,16 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] bg-gradient-to-r text-white">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/aristocrats-logo.svg"
-          alt="Aristocrats logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <h1 className="text-4xl font-bold text-center sm:text-left">
+        <h1 className="text-6xl font-extrabold text-center sm:text-left drop-shadow-lg">
           Aristocrats
         </h1>
-        <p className="text-lg text-center sm:text-left">
+        <p className="text-2xl text-center sm:text-left italic">
           Crafting spaces that inspire. Coming soon.
         </p>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://aristocrats.com/contact"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/contact.svg"
-            alt="Contact icon"
-            width={16}
-            height={16}
-          />
-          Contact Us
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://aristocrats.com/portfolio"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/portfolio.svg"
-            alt="Portfolio icon"
-            width={16}
-            height={16}
-          />
-          Our Portfolio
-        </a>
+        <p className="text-lg">Stay tuned for more updates!</p>
       </footer>
     </div>
   );
